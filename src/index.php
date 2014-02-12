7807efcb04ffd899f804bf1a5beee9835124169b
ArkProject <jun@jun-pc>
2014-02-12 23:58:50 +0800

    test

--- src/index.php
+++
@@ -1,8 +0,0 @@
-﻿<?php
-define('SECURITY_DIR',dirname(__FILE__));
-require './framework/ark.php';
-
-//application starting
-ark_start(include ROOT_DIR.'data/config.php');
-
-?>
---
+++ src/web/index.php
@@ -0,0 +1,11 @@
+﻿<?php
+
+echo 'fgdfg';
+/*
+define('SECURITY_DIR',readdir(dirname(__FILE__).'/..'));
+require '../framework/ark.php';
+
+//application starting
+ark_start(include ROOT_DIR.'data/config.php');
+*/
+?>
