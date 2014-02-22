4ad563941279595a13e172bc5f593a9ca69c022e
jun@jun-pc <jun@jun-pc>
2014-02-23 00:13:47 +0800

    更新说明文档

--- apps/myapp/App.class.php
+++ apps/myapp/App.class.php
@@ -3,7 +3,7 @@ namespace myapp;
 defined ( 'ARK' ) or exit ( 'access denied' );
 class App extends \ark\Application{
 	function init(){
-		echo '<h1>im in here </h1>';
+		echo '<h1>im in here</h1>';
 		parent::init();
 	}
 }
